3a59895d0be1ea20e4c30217e37f8ed2d2dfb0ec
d4rthfaded20 <ruiz.fam2016@outlook.com>
2024-09-10 09:18:11 -0700

    9/10/2024
    Classwork for september 10

---
+++ Mad_lib.py
@@ -0,0 +1,12 @@
+noun1 = input("Enter a noun: ")
+pnoun = input("Choose a plural noun: ")
+noun = input("Enter a noun: ")
+place = input("Name a place: ")
+adjective = input("Enter an adjective: ")
+
+
+print("I went to go cash my" + noun1 )
+print("i took" + pnoun, "with me" )
+print("otw i got some" + noun )
+print("When im cashed out im going to" + place )
+print("Now that im here i realize its so" + adjective )
---
+++ hangman.py
@@ -0,0 +1,1 @@
+# Build a hangman game here
