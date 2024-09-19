5074d14bd769a25cbdb69ccfdfd5d56af4a0dd6a
d4rthfaded20 <ruiz.fam2016@outlook.com>
2024-09-19 10:31:52 -0700

    sad
    asd

--- hangman.py
+++ hangman.py
@@ -1,1 +1,45 @@
+import random
+from words import words_list
+
 # Build a hangman game here
+###print(words_list)
+
+hangman_art = {0:("    ",
+                  "    ",
+                  "    "),
+               1: (" o  ",
+                  "     ",
+                  "     "),
+               2: (" o  ",
+                   " |  ",
+                   "    "),
+               3: (" o  ",
+                  "  |  ",
+                  " /   "),
+               4: ("  o  ",
+                   "  |\\  ",
+                   " /   "),
+               5: ("  o  ",
+                   "  |\\  ",
+                   " / \\  "),
+               6: ("  o  ",
+                   " /|\\  ",
+                   " / \\  ")}
+word = random.choice(words_list)
+tries = 6
+
+print (word)
+for line in hangman_art[0: +tries]:
+    print (line)
+
+def display_man(wrong_guesses):
+    pass
+
+def display_hint(hint):
+    pass
+
+def display_answer(answer):
+    pass
+
+
+if input(tries) == (word):
---
+++ words.py
@@ -0,0 +1,6 @@
+words_list = [
+    "ebullient", "ubiquitous", "pernicious", "sycophant", "ephemeral",
+    "quintessential", "altruistic", "meticulous", "aplomb", "polemical",
+    "obfuscate", "conundrum", "eloquent", "magnanimous", "serendipity",
+    "juxtapose", "disparate", "epiphany", "voracious", "recalcitrant"
+]
